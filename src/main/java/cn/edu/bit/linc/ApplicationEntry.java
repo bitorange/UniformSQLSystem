0826d2bd94f55400bec92087a7a3c1b0bdee5701
ihainan <ihainan72@gmail.com>
2015-06-25 10:49:47 +0800

    Add: first commit

---
+++ src/main/java/cn/edu/bit/linc/ApplicationEntry.java
@@ -0,0 +1,10 @@
+package cn.edu.bit.linc;
+
+/**
+ * Created by ihainan on 6/24/15.
+ */
+public class ApplicationEntry {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
