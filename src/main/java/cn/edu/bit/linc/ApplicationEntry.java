76b08602eb97844b0cb8e1142c91f31190c79ccc
ihainan <ihainan72@gmail.com>
2015-06-29 01:26:08 +0800

    Add: 外部配置文件支持

--- src/main/java/cn/edu/bit/linc/ApplicationEntry.java
+++ src/main/java/cn/edu/bit/linc/ApplicationEntry.java
@@ -1,10 +1,31 @@
 package cn.edu.bit.linc;
 
 /**
- * Created by ihainan on 6/24/15.
+ * 应用入口
  */
 public class ApplicationEntry {
+    /**
+     * 解析程序运行参数，读取配置文件
+     *
+     * @param args 程序参数
+     */
+    private static void readConfigureFile(String[] args) {
+        GlobalVar.parseArgs(args);
+    }
+
+    /**
+     * 主函数
+     * @param args 程序参数
+     */
     public static void main(String[] args){
-        System.out.println("Hello World");
+        // 解析程序运行参数，读取配置文件
+        ApplicationEntry.readConfigureFile(args);
+        GlobalVar.printConf();
+
+        // 系统初始化
+
+        // 读取 ZQL 语句
+
+        // 分别执行 ZQL 语句，输出结果
     }
 }
---
+++ src/main/java/cn/edu/bit/linc/GlobalVar.java
@@ -0,0 +1,75 @@
+package cn.edu.bit.linc;
+
+import java.util.HashMap;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * 全局系统变量 <br />
+ */
+public class GlobalVar {
+    public static HashMap<String, String> configMap = new HashMap<String, String>();
+
+    /**
+     * 默认配置值
+     */
+    static {
+        // TODO: 设置默认配置值
+    }
+
+    public static void parseArgs(String args[]) {
+        // 解析程序运行参数
+        ArgumentParser parser = ArgumentParsers.newArgumentParser("console").defaultHelp(true).description("Spark SQL Console By LINC");
+        parser.addArgument("-c", "--config")
+                .setDefault("./conf.xml")
+                .help("specify the system configuration file");
+
+        Namespace ns = null;
+        try {
+            ns = parser.parseArgs(args);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+
+        String configureFilePath = ns.getString("config");
+        try {
+            GlobalVar.readConFile(configureFilePath);
+        } catch (Exception e) {
+            System.err.println("解析配置文件失败，失败原因：" + e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * 读取配置文件
+     *
+     * @param filePath 配置文件路径
+     * @throws ConfigurationException 读取 XML 配置文件失败
+     */
+    private static void readConFile(String filePath) throws ConfigurationException {
+        XMLConfiguration config = new XMLConfiguration(filePath);
+        NodeList list = config.getDocument().getElementsByTagName("entry");
+        for (int i = 0; i < list.getLength(); i++) {
+            Node node = list.item(i);
+            String key = node.getAttributes().getNamedItem("key").getTextContent();
+            String val = node.getTextContent();
+            configMap.put(key, val);
+        }
+    }
+
+    /**
+     * 输出配置项
+     */
+    public static void printConf(){
+        for (String key: configMap.keySet()){
+            System.out.println(key + ": " + configMap.get(key));
+        }
+    }
+}
